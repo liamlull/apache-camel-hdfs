b5b8d168b7cca94083c795ac5768ac41ed07b127
marius cornescu <marius_cornescu@yahoo.com>
2019-09-11 22:03:18 +0200

    unit tests

--- src/test/java/org/apache/camel/component/hdfs/kerberos/HdfsKerberosConfigurationFactoryLegacyTest.java
+++ src/test/java/org/apache/camel/component/hdfs/kerberos/HdfsKerberosConfigurationFactoryLegacyTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import java.io.IOException;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.*;
 
 public class HdfsKerberosConfigurationFactoryLegacyTest {
@@ -13,15 +15,16 @@ public class HdfsKerberosConfigurationFactoryLegacyTest {
     @Test
     public void setupKerberosHdfsConfiguration() throws IOException {
         // given
-        Configuration hdfsConfiguration = null;
-        String namedNodes = null;
-        String kerberosConfigFileLocation = null;
+        Configuration hdfsConfiguration = new Configuration();
+        String namedNodes = "node1:1234,node2:5678";
+        String kerberosConfigFileLocation = "/some/file/path";
         int replicationFactor = 3;
 
         // when
         HdfsKerberosConfigurationFactoryLegacy.setupKerberosHdfsConfiguration(hdfsConfiguration, namedNodes, kerberosConfigFileLocation, replicationFactor);
 
         // then
+        assertThat(hdfsConfiguration, notNullValue());
 
     }
 
